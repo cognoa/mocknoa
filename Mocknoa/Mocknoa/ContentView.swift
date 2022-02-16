f678b68981228bdd8f251e509c73761c196c1294
Wilma Rodriguez <kima_92@fastmail.com>
2022-02-16 14:09:35 -0500

    Display endpoints of a selected server

--- Mocknoa/Mocknoa/ContentView.swift
+++ Mocknoa/Mocknoa/ContentView.swift
@@ -9,19 +9,21 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State var currentServer = Server(name: "Server 331", port: 0331)
+    @State var currentServer = Server(name: "Server 331", port: 0331, endpoints: [])
 
     let servers: [Server] = [
-        .init(name: "Server 331", port: 0331),
-        .init(name: "Server 332", port: 0332),
-        .init(name: "Server 333", port: 0333),
-        .init(name: "Server 334", port: 0334),
+        .init(name: "Server 331", port: 0331, endpoints: []),
+        .init(name: "Server 332", port: 0332, endpoints: []),
+        .init(name: "Server 333", port: 0333, endpoints: []),
+        .init(name: "Server 334", port: 0334, endpoints: []),
     ]
 
     var body: some View {
         NavigationView {
+            // List of Servers
             SidebarView(currentServer: $currentServer, servers: servers)
-            ServerConfigurationPane()
+            // List of currentServer's endpoints
+            ServerConfigurationPane(currentServer: $currentServer)
             JSONInputTextEditor()
         }
         // Allows the mac app window to be resizable,
@@ -34,6 +36,12 @@ struct SidebarView: View {
     @State private var isDefaultItemActive = true
     @Binding var currentServer: Server
     let servers: [Server]
+
+    private func getDummyEndpoints() -> [Endpoint] {
+        [.init(path: "somePath1", action: .get, statusCode: 555, jsonString: "JSON string"),
+         .init(path: "somePath2", action: .post, statusCode: 555, jsonString: "JSON string"),
+         .init(path: "somePath3", action: .delete, statusCode: 555, jsonString: "JSON string")]
+    }
 
     var body: some View {
         VStack {
@@ -49,6 +57,9 @@ struct SidebarView: View {
                     if currentServer != server {
                         currentServer = server
                     }
+                    if currentServer == servers[1] {
+                        currentServer.endpoints = getDummyEndpoints()
+                    }
                 }
             }
             .listStyle(SidebarListStyle()) // Gives you this sweet sidebar look
@@ -57,15 +68,37 @@ struct SidebarView: View {
     }
 }
 
-
+/// Lists the currently selected server's endpoints
 struct ServerConfigurationPane: View {
+    @Binding var currentServer: Server
+
+    /*
+     public struct Endpoint: Codable, Hashable {
+     public var path: String
+     public var action: HttpAction
+     public var statusCode: UInt
+     public var jsonString: String
+     }
+     */
+
     var body: some View {
-        HttpActionPicker()
+        ForEach(currentServer.endpoints, id:\.self) { endpoint in
+            VStack {
+                HStack {
+                    Text(endpoint.path)
+                    Spacer()
+                }
+                HttpActionPicker(httpAction: endpoint.action)
+            }
+            .padding()
+        }
+        Spacer()
     }
 }
 
 struct HttpActionPicker: View {
     @State var httpAction: HttpAction = .get
+
     var body: some View {
         Picker("Http Action", selection: $httpAction) {
             ForEach(HttpAction.allCases) { action in
@@ -80,5 +113,10 @@ struct JSONInputTextEditor: View {
     var body: some View {
         Text("Input JSON Below")
         TextEditor(text: $text)
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
--- Mocknoa/Mocknoa/Models/Server.swift
+++ Mocknoa/Mocknoa/Models/Server.swift
@@ -10,6 +10,5 @@ import Foundation
 public struct Server: Codable, Hashable {
     public var name: String
     public var port: UInt
-
-    
+    public var endpoints: [Endpoint]
 }
